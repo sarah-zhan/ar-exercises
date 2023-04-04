a1315dfdfbad55501735aaade8723eb33fb7f49f
sarah-zhan <yizh.contact@gmail.com>
2023-04-04 13:20:18 -0700

    update store1 record

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,7 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find('1')
+@store2 = Store.find('2')
+# pp @store1.id
+Store.update(@store1.id, annual_revenue: 330_000)
